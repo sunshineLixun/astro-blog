71c98b457c4c818114d4e91d0913ec585708c580
Hello Li <1261142605@qq.com>
2024-03-30 22:06:39 +0800

    feat: add RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,20 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
 
 export async function GET(context) {
+  const postImportResult = import.meta.glob("../content/posts/**/*.mdx", {
+    eager: true,
+  });
+  const posts = Object.values(postImportResult);
+
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob("../content/posts/**/*.mdx")
-    ),
-    customData: `<language>en-us</language>`,
+    items: posts.map((post) => ({
+      title: post.frontmatter.title,
+      description: post.frontmatter.spoiler,
+      link: post.frontmatter.href,
+      pubDate: new Date(post.frontmatter.date),
+    })),
   });
 }
